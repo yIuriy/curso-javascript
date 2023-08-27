2cfcc1905b0f3d8d43ded09edda02c394c23c583
Iuri <133531559+yIuriy@users.noreply.github.com>
2023-08-27 19:05:32 -0300

    Realizei os exe 017 e 018

--- exercicios/exe017/main.js
+++ exercicios/exe017/main.js
@@ -8,13 +8,29 @@ var res = document.querySelector("div#res");
 function contar() {
   res.innerHTML = "";
   var inicio = Number(txtinicio.value);
+  console.log(inicio);
   var fim = Number(txtfim.value);
+  console.log(fim);
   var passo = Number(txtpasso.value);
-  if (inicio <= fim) {
-    while (inicio <= fim) {
-      res.innerHTML += ` ${inicio} →`;
-      inicio += passo;
+  console.log(passo);
+  if (passo == 0) {
+    window.alert("Passo inválido! Passo considera será 1.");
+    passo = 1;
+  }
+  if (passo == "" || passo == null) {
+    res.innerHTML = "Impossível contar";
+  } else if (fim == "" || fim == null) {
+    res.innerHTML = "Impossível contar";
+  } else if (passo == 0) {
+    window.alert("Passo inválido! Passo considerado será 1.");
+    passo = 1;
+  } else {
+    if (inicio <= fim) {
+      while (inicio <= fim) {
+        res.innerHTML += ` ${inicio} →`;
+        inicio += passo;
+      }
+      res.innerHTML += " Fim!";
     }
-    res.innerHTML += " Fim!";
   }
 }
---
+++ exercicios/exe018/main.js
@@ -0,0 +1,24 @@
+var txtvalor = document.getElementById("valor");
+var txtlimite = document.getElementById("limite");
+var btn = document.getElementById("btn");
+var res = document.getElementById("res");
+
+function tabuada() {
+  res.innerHTML = "";
+  var valor = Number(txtvalor.value);
+  var limite = Number(txtlimite.value);
+  console.log("Working!");
+
+  res.innerHTML += `<th>Tabuada do ${valor}</th>`;
+  if (limite == "" || limite == null) {
+    limite = 10;
+  }
+  if (valor == "" || valor == null) {
+    res.innerHTML = "Insira um valor!";
+  } else {
+    for (var c = 0; c < limite + 1; c++) {
+      var tabuada = valor * c;
+      res.innerHTML += `<td>${valor} x ${c} = ${tabuada}</td>`;
+    }
+  }
+}
